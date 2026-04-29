6f5ca639fc04c17c51f1e3d219484c666e275803
Shwetha <koppashwethav@gmail.com>
2026-04-29 11:29:37 +0530

    Added better UI design

---
+++ src/main/java/com/example/expense_tracker/controller/AuthController.java
@@ -0,0 +1,47 @@
+
+package com.example.expense_tracker.controller;
+
+import com.example.expense_tracker.model.User;
+import com.example.expense_tracker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Register
+    @PostMapping("/register")
+    public Map<String, String> register(@RequestBody User user) {
+        Map<String, String> response = new HashMap<>();
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            response.put("message", "Email already exists!");
+            return response;
+        }
+        userRepository.save(user);
+        response.put("message", "Registration successful!");
+        return response;
+    }
+
+    // Login
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestBody User user) {
+        Map<String, String> response = new HashMap<>();
+        Optional<User> found = userRepository.findByEmail(user.getEmail());
+        if (found.isPresent() && found.get().getPassword().equals(user.getPassword())) {
+            response.put("message", "Login successful!");
+            response.put("userId", found.get().getId().toString());
+            response.put("name", found.get().getName());
+        } else {
+            response.put("message", "Invalid email or password!");
+        }
+        return response;
+    }
+}
